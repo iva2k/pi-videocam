bae1b9e866acc03db25a33db2f401f354c5d3d82
iva2k <iva2k@yahoo.com>
2019-09-13 14:29:00 -0700

    Change install.sh to edit /boot/cmdline.txt instead of replace

--- webcam-video.gstreamer/install.sh
+++ webcam-video.gstreamer/install.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+CONFIG=/boot/config.txt
+
+is_pi () {
+  ARCH=$(dpkg --print-architecture)
+  if [ "$ARCH" = "armhf" ] ; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+if is_pi ; then
+  CMDLINE=/boot/cmdline.txt
+else
+  CMDLINE=/proc/cmdline
+fi
+
+# Must be on pi
+if ! is_pi ; then
+  printf "Only Raspberry Pi is supported.\n"
+  exit 1
+fi
+
 # Must be root
 if [ $(id -u) -ne 0 ]; then
   printf "Must be root. Did you use 'sudo'?\n"
@@ -24,7 +47,17 @@ sudo systemctl start webcam-video.service
 sudo systemctl enable webcam-video.service
 sudo systemctl enable splashscreen.service
 
-printf "\nPrevent screen blanking...\n"
+printf "Adjusting settings...\n"
 echo 0 | sudo tee /sys/class/graphics/fb0/blank >/dev/null
+sudo cp $CMDLINE $CMDLINE.bak
+sed -i $CMDLINE -e "s/ quiet//"
+sed -i $CMDLINE -e "s/ splash//"
+sed -i $CMDLINE -e "s/ plymouth.ignore-serial-consoles//"
+sed -i $CMDLINE -e "s/ plymouth.enable=[0-1]//"
+sed -i $CMDLINE -e "s/ loglevel=[0-9]//"
+sed -i $CMDLINE -e "s/ logo.nologo//"
+sed -i $CMDLINE -e "s/ vt.global_cursor_default=[0-1]//"
+sed -i $CMDLINE -e "s/$/ quiet splash plymouth.ignore-serial-consoles/"
+sed -i $CMDLINE -e "s/$/ plymouth.enable=0 loglevel=3 logo.nologo vt.global_cursor_default=0/"
 
-printf "\nDONE.\n"
+printf "\nDone.\n"
